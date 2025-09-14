3fcad56e34687e68ea7a129c46f123a63009c3ba
Harsh Kumar <yadavharsh1201@gmail.com>
2025-09-14 14:17:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='font-bold text-6xl text-white'>Todo RTK</h1>
+    </>
+  )
+}
+
+export default App
