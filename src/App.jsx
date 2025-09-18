819ed30fce0b592d51a1b464aa67e4d7c41e865a
Harsh Kumar <yadavharsh1201@gmail.com>
2025-09-18 19:30:11 +0530

    Add functionality to remove todos and update AddTodo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
+import { Provider } from 'react-redux'
+import {store} from './app/store'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='font-bold text-6xl text-white'>Todo RTK</h1>
-    </>
-  )
+    <Provider store={store}>
+      <h1 className="font-bold text-6xl text-white">Todo RTK</h1>
+      <AddTodo />
+      <Todos />
+    </Provider>
+  );
 }
 
 export default App
